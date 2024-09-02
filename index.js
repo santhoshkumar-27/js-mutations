5c788e838949ea266781d7862b757d4943a095ba
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-02 18:56:42 +0530

    fix: mutation observor added

---
+++ index.js
@@ -0,0 +1,58 @@
+(function (win) {
+    'use strict';
+
+    var listeners = [],
+        doc = win.document,
+        MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+        observer;
+
+    function ready(selector, fn) {
+        // Store the selector and callback to be monitored
+        listeners.push({
+            selector: selector,
+            fn: fn
+        });
+        if (!observer) {
+            // Watch for changes in the document
+            observer = new MutationObserver(check);
+            observer.observe(doc.documentElement, {
+                childList: true,
+                subtree: true
+            });
+        }
+        // Check if the element is currently in the DOM
+        check();
+    }
+
+    function check() {
+        // Check the DOM for elements matching a stored selector
+        for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+            listener = listeners[i];
+            // Query for elements matching the specified selector
+            elements = doc.querySelectorAll(listener.selector);
+            for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+                element = elements[j];
+                if (!element.ready) {
+                    element.ready = [];
+                }
+                // Make sure the callback isn't invoked with the
+                // same listener more than once
+                // due to other mutations
+                if (!element.ready[i]) {
+                    element.ready[i] = true;
+                    // Invoke the callback with the element
+                    listener.fn.call(element, element);
+                }
+            }
+        }
+    }
+
+    // Expose 'ready'
+    win.wrapperForMutationObservor = ready;
+
+})(this);
+
+
+window.wrapperForMutationObservor('.selector', function (ele) {
+    callback(ele);
+});
